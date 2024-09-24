298f769926e0f990de7a22463615a13cb997d657
Dani Crunch <daniel@drywa.me>
2024-09-24 14:26:50 +0100

    Changing the license to a modified zlib license.

--- README.md
+++ README.md
@@ -5,14 +5,16 @@ The only dependency for most of these files is the base header which contains al
 If a file has any other dependencies they will be listed in the comment section at the beginning of the file.
 
 
-## Platforms
+## Info
 
-All these files are specifically written for the `Microsoft (R) C/C++ Optimizing Compiler Version 19.38.33135 for x64` and have only been tested on `Windows 11 Home 64-bit - 23H2 - 22631.4169`. 
+Build Platform: `Windows 11 Home 64-bit - 23H2 - 22631.4169`
+
+Compiler: `Microsoft (R) C/C++ Optimizing Compiler Version 19.38.33135 for x64`
 
 
 ## How to use
 
-Simply select the library you would like and copy it to your projects source code folder. Additional configuration can be done by either editing the file or by supplying known definitions before including. See each files comment section at the beginning of the file to find out more about all the different available configurations.
+Select the library you would like to use and copy it to your projects source code directory. Additional configuration can be done by either editing the file or by supplying known definitions before including. See the comment section at the beginning of each library file to find out more about all the different available configurations.
 
 
 ## Libraries
@@ -27,4 +29,4 @@ The following libraries are available:
 
 ## License
 
-Each library has licence information included at the end of the file. It is a modified BSD licence which gives you a lot of freedom on how to use the code. If you get any use out of these libraries please consider donating to the creator at https://ko-fi.com/danicrunch
+Each library has licence information included at the end of the file. It is based on the zlib license and can also be found [here](LICENSE).
--- src/dani_base.h
+++ src/dani_base.h
@@ -3,7 +3,7 @@
 //
 // Author: Dani Drywa (dani@drywa.me)
 //
-// Last change: 2024/09/19 (yyyy/mm/dd)
+// Last change: 2024/09/24 (yyyy/mm/dd)
 //
 // License: See end of file
 //
@@ -127,18 +127,24 @@ typedef u32 b32;
 /*
 Danilib - dani_base.h License:
 ---------------------------------------------------------------------------------
-Copyright 2024 Dani Drywa (dani@drywa.me)
+Copyright (c) 2024 Dani Drywa (dani@drywa.me)
 
-Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+This software is provided 'as-is', without any express or implied
+warranty. In no event will the authors be held liable for any damages
+arising from the use of this software.
 
-1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
 
-2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-
-3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
-
-4. If the source or binary form was of value to you, consider donating to the creator at https://ko-fi.com/danicrunch
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+1. The origin of this software must not be misrepresented; you must not
+   claim that you wrote the original software. If you use this software
+   in a product, an acknowledgment in the product documentation would be
+   appreciated but is not required.
+2. Altered source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+3. This notice may not be removed or altered from any source distribution.
+4. If you use this software in a product, a donation to the original author
+   (https://ko-fi.com/danicrunch) would be appreciated but is not required.
 ---------------------------------------------------------------------------------
 */
